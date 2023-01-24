30e44d55cdd54c23000e3bd8b8ba094580cfa107
andre-bandeli <andre.lbandeli@gmail.com>
2023-01-24 03:32:08 -0300

    refactor: removendo modulos nao usados

--- lead/src/main/java/com/lead/lead/repository/LeadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lead.lead.repository;
-
-import com.lead.lead.model.Lead;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeadRepository extends CrudRepository<Lead, Long> {
-}
