ab68f44c80c3c138690f1f6eac960bd5488f4fd4
andre-bandeli <andre.lbandeli@gmail.com>
2023-02-01 11:24:02 -0300

    refactor: mudando fonte  pagle title veiculos + sobre + motos + contato

--- page/src/paginas/sobre/PageTitle.jsx
+++ page/src/paginas/sobre/PageTitle.jsx
@@ -3,8 +3,8 @@ import './pageTitle.scss'
 
 export default function PageTitle() {
   return (
-    <div className='title_sobre'>
-        <h2>CONHEÇA NOSSA HISTÓRIA</h2>
+    <div className='title_financeiro'>
+        <h2>Sobre Nós</h2>
     </div>
   )
 }
--- page/src/paginas/sobre/header/HeaderSobre.jsx
+++ page/src/paginas/sobre/header/HeaderSobre.jsx
@@ -4,7 +4,7 @@ import './headerSobre.scss'
 export default function HeaderSobre() {
   return (
     <div className='headerSobre'>
-
+        <h1>Conheça nossa <span>História</span></h1>
     </div>
   )
 }
